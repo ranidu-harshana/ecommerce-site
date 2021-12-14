13ae320bae4760265796c4f3328679f600c5b21d
ranidu-harshana <harshanaranidum@gmail.com>
2021-12-15 00:48:47 +0530

    call get categories function

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,10 +1,4 @@
 <p class="lead">Shop Name</p>
 <div class="list-group">
-    <?php
-        $sql = "SELECT * FROM categories";
-        $result = query($sql);
-        while ($row = fetch_array($result)) {
-            echo '<a href="" class="list-group-item">'. $row['cat_title'] .'</a>';
-        }
-    ?>
+    <?php get_categories(); ?>
 </div>
