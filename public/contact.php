384b49e6b369df70a49e1803912a8cc64fd79b60
ranidu-harshana <harshanaranidum@gmail.com>
2021-12-15 14:06:00 +0530

    Make some changes

--- public/contact.php
+++ public/contact.php
@@ -1,16 +1,11 @@
+<?php require_once("../resources/config.php") ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php") ?>
+<body>
 
-<!-- Configuration-->
-
-<?php require_once("../resources/config.php"); ?>
-
-
-<!-- Header-->
-<?php include(TEMPLATE_FRONT .  "/header.php");?>
-
-
-     <!--Navigation -->
-
-<?php include(TEMPLATE_FRONT .  "/top_nav.php");?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <?php include(TEMPLATE_FRONT . DS . "top_nav.php") ?>
+    </nav>
 
          <!-- Contact Section -->
 
@@ -23,25 +18,25 @@
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <form name="sentMessage" id="contactForm" >
+                    <form name="sentMessage" id="contactForm" action="" method="POST">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <input type="text" class="form-control" placeholder="Your Name *" name="name" required data-validation-required-message="Please enter your name.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <input type="email" class="form-control" placeholder="Your Email *" name="email" required data-validation-required-message="Please enter your email address.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                                 <div class="form-group">
-                                    <input type="tel" class="form-control" placeholder="Your Phone *" id="phone" required data-validation-required-message="Please enter your phone number.">
+                                    <input type="tel" class="form-control" placeholder="Your Phone *" name="subject" required data-validation-required-message="Please enter your phone number.">
                                     <p class="help-block text-danger"></p>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <textarea class="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <textarea class="form-control" placeholder="Your Message *" name="message" required data-validation-required-message="Please enter a message."></textarea>
                                     <p class="help-block text-danger"></p>
                                 </div>
                             </div>
--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,17 @@
 <?php
-    
+    function set_message($msg) {
+        if(!empty($msg)) {
+            $_SESSION['message'] = $msg;
+        }
+    }
+
+    function display_message() {
+        if(isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        }
+    }
+
     function redirect($location){
         header("Location: $location");
     }
@@ -83,6 +95,24 @@
             DELIMETER;
 
             echo $product;
+        }
+    }
+
+    function login_user() {
+        if(isset($_POST['submit'])) {
+            $username = escape($_POST['username']);
+            $password = escape($_POST['password']);
+
+            $query = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'";
+            $result = query($query);
+            confirm($result);
+
+            if(mysqli_num_rows($result) == 0) {
+                set_message("Username or Password is incorrect");
+                redirect('login.php');
+            }else{
+                redirect('admin');
+            }
         }
     }
 ?>
